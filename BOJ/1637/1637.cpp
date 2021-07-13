e7267a0b613d673f8956d3b229df4d04321f063e
홍지환 <tjtks24@naver.com>
2021-07-13 14:16:53 +0000

    1637/맞음

---
+++ BOJ/1637/1637.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+
+using namespace std;
+
+const int maxN = 20050;
+int n;
+long long int a[maxN], c[maxN], b[maxN];
+
+bool isOdd(long long int x) {
+	if(x%2==1) return true;
+	return false;
+}
+
+long long int floor(int i, long long int lim) {
+	return ((lim-a[i])/b[i])*b[i]+a[i];
+}
+
+long long int countRange(long long int left, long long int right) {
+	long long cnt = 0;
+	long long int lVal, rVal;
+	long long int lLim, rLim;
+	for(int i = 1; i <= n; i++) {
+		                     
+		lLim = max(left, a[i]); rLim = min(right,floor(i,c[i]));
+		lVal = floor(i, lLim);
+		if(lVal<left) lVal+=b[i];
+		rVal = floor(i,rLim);
+		
+		if(rVal >= lVal && rLim >= lLim) cnt+=(rVal-lVal)/b[i]+1;
+		
+		//cout<<"cR "<<left<<" "<<right<<" i is "<<i<<" cnt is "<<cnt<<endl;
+	}
+	return cnt;
+}
+
+int main ( void ) {
+	
+	freopen("/workspace/PS_Git/BOJ/1637/input.txt","r",stdin);
+	long long int left=2147483648, right=0, mid;
+	
+	scanf("%d", &n);
+	for(int i=1; i <=n; i++) {
+		scanf("%lld %lld %lld", &a[i], &c[i], &b[i]);
+		left=min(left,a[i]);
+		right=max(right,c[i]);
+	}
+	
+	if(!isOdd(countRange(left,right))) {
+		printf("NOTHING\n");
+		return 0;
+	}
+	mid=(left+right)/2;
+	
+	while(left!=right) {
+		
+		if(isOdd(countRange(left,mid))) {
+			right=mid;
+			mid=(left+right)/2;
+		}
+		else {
+			left=mid+1;
+			mid=(left+right)/2;
+		}
+	}
+	
+	cout<<left<<" "<<countRange(left,right)<<endl;
+	
+}
