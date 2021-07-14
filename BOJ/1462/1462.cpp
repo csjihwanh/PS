06ee5f54651021b83828e2fcb8acec2436a9018c
홍지환 <tjtks24@naver.com>
2021-07-14 03:00:32 +0000

    1462/ 맞았다

--- BOJ/1462/1462.cpp
+++ BOJ/1462/1462.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#define NINF -98765432198
 
 using namespace std;
 
+typedef long long int ll;
 const int maxN = 500500;
-
 int n, m;
 int score[maxN], bonus[maxN];
-int dp[maxN][3]; //i번째 문제까지 풀고 j개 coin을 갖고 있을 때의 점수의 max
+ll scoreAcc[maxN], zeroScore[maxN];
 
-int main ( void ) {
+ll calcMaxScore() {
+	ll bonusResult = 0;
+	ll maxScore= 0;
+	int maxIndex = 0;
+	for(int i = 1; i<= n; i++) {
+		if(i<m) bonusResult = NINF;
+		else bonusResult = zeroScore[i-m]+scoreAcc[i]-scoreAcc[i-m]+bonus[i];
+		zeroScore[i] = max(bonusResult, maxScore-score[i]);
+		if(bonusResult>=maxScore+score[i]) {
+			maxScore = bonusResult;
+			maxIndex = 0;
+		}
+		else {
+			maxScore += score[i];
+			maxIndex++;
+		}
+	}
+	return max(zeroScore[n],maxScore);
+}
+
+int main(void) {
+	freopen("/workspace/PS_Git/BOJ/1462/input.txt", "r", stdin);
 	
 	scanf("%d %d", &n, &m);
-	for(int i = 1; i <= n; i++) scanf("%d", &score[i]);
-	for(int i= 1; i<=n; i++) scanf("%d", &bonus[i]);
-	
-	dp[1][0]=-score[1], dp[1][1] = score[1];
-	for(int i= 2; i<=n; i++) {
-		dp[i][0] = max(max(dp[i-1][0]-score[i], dp[i-1][1]-score[i]),dp[i-1][2]+score[i]+bonus[i]);
-		dp[i][1] = dp[i-1][0]+score[i];
-		dp[i][2] = dp[i-1][1]+score[i];
+	for(int i = 1; i <= n; i++) {
+		scanf("%d", &score[i]);
+		scoreAcc[i]=scoreAcc[i-1]+(ll)score[i];
 	}
+	for(int i = 1; i <= n; i++) scanf("%d", &bonus[i]);
 	
-	cout<<max(max(dp[n][0],dp[n][1]),dp[n][2])<<endl;
-	return 0;
+	cout<<calcMaxScore()<<endl;
 }
