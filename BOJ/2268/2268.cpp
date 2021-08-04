35a7555354869e951fa0fd94c9d480a1e233439f
홍지환 <tjtks24@naver.com>
2021-08-04 10:17:38 +0000

    2268/210804/AC

---
+++ BOJ/2268/2268.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long int ll;
+const int maxN = 1000500;
+vector<ll> arr;
+vector<ll> tree(maxN*4);
+
+ll init(int left, int right, int node) {
+	if(left==right) return tree[node]=arr[left];
+	int mid= (left+right)/2;
+	return tree[node] = init(left,mid,node*2) + init(mid+1,right,node*2+1);
+}
+
+void update(int left, int right, int node, ll diff, int index) {
+	if(index<left || index > right) return;
+	tree[node] +=diff;
+	if(left==right) return;
+	int mid = (left+right)/2;
+	update(left,mid,node*2,diff,index);
+	update(mid+1,right,node*2+1,diff,index);
+}
+
+ll query(int left, int right, int node, int qleft, int qright) {
+	if(qleft > right || qright < left) return 0;
+	if(left>=qleft && right<=qright) return tree[node];
+	int mid = (left+right)/2;
+	return query(left,mid,node*2,qleft,qright)+query(mid+1,right,node*2+1,qleft,qright);
+}
+
+void swap(ll* a, ll* b) {
+	ll temp;
+	if(*a > *b) {
+		temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
+
+int main ( void ) {
+	cin.tie(NULL);cout.tie(NULL);ios_base::sync_with_stdio(false);
+	freopen("/workspace/PS_Git/BOJ/2268/input.txt","r",stdin);
+	
+	int n,q;
+	ll temp1,temp2;
+	
+	cin>>n>>q;
+	
+	arr.emplace_back(0);
+	for(int i = 1; i<=n; i++) {
+		arr.emplace_back(0);
+	}
+	init(1,n,1);
+	
+	for(int i =1; i<=q; i++) {
+		cin>>temp1;
+		if(temp1==0) {
+			cin>>temp1>>temp2;
+			cout<<query(1,n,1,temp1,temp2)<<'\n';
+		}
+		else if(temp1==1) {
+			cin>>temp1>>temp2;
+			update(1,n,1,temp2-arr[temp1],temp1);
+			arr[temp1]= temp2;
+		}
+	}
+	return 0;
+}
