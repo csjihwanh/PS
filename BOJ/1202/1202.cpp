79e15ca910ceb294c25d8d45f786a85995c319c0
홍지환 <tjtks24@naver.com>
2021-08-31 06:21:41 +0000

    RE / InvalidPointer

---
+++ BOJ/1202/1202.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+typedef struct item_s {
+	int mass, value; 
+}ITEM_S;
+typedef long long int ll;
+struct comp_value {
+	bool operator()(ITEM_S a, ITEM_S b) {
+		return (a.value < b.value);
+	}
+};
+bool comp_mass (ITEM_S a, ITEM_S b){
+	return (a.mass < b.mass);
+};
+
+vector<ITEM_S> item;
+vector<int> bag;
+priority_queue<ITEM_S, vector<ITEM_S>, comp_value> ableItem;
+int n,k;
+ll answer = 0;
+
+ITEM_S makeItem(int myMass, int myValue) {
+	ITEM_S myItem;
+	myItem.mass = myMass;
+	myItem.value = myValue;
+	return myItem;
+}
+
+int main(void) {
+	cin.tie(NULL);cout.tie(NULL);ios_base::sync_with_stdio(false);
+	freopen("/workspace/PS_Git/BOJ/input.txt","r",stdin);
+	
+	int temp1, temp2;
+	cin>>n>>k;
+	for(int i = 1; i <= n; i++) {
+		cin>>temp1>>temp2;
+		item.push_back(makeItem(temp1,temp2));
+	}
+	for(int i = 1; i <= k; i++) {
+		cin>>temp1;
+		bag.push_back(temp1);
+	}
+	sort(bag.begin(),bag.end());
+	sort(item.begin(), item.end(), comp_mass);
+	
+	int bagIdx = 0;
+	for(int i = 0; i < k; i++) {
+		int curBag = bag[i];
+		if(bagIdx < item.size()-1) {
+			while(item[bagIdx].mass <= curBag) {
+				ableItem.push(item[bagIdx++]);
+			}
+		}
+		
+		ITEM_S maxItem = ableItem.top();
+		answer+= maxItem.value;
+		ableItem.pop();
+	}
+	
+	cout<<answer;
+	
+	return 0;
+}
